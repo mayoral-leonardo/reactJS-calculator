6da2930f86ad153d8e41c8a9a12782dcaf43b863
Leonardo <mayoral.leonardo99@gmail.com>
2023-03-03 11:02:33 -0300

    removed unused code

--- src/views/Calculator/functions.ts
+++
@@ -1,8 +0,0 @@
-import { _mathOperations } from './types'
-
-export const calculatorFunctions: {[key in _mathOperations]: Function} = {
-  add: (firstNumber: number, secondNumber: number) => firstNumber + secondNumber,
-  substraction: (firstNumber: number, secondNumber: number) => firstNumber - secondNumber,
-  multiplication: (firstNumber: number, secondNumber: number) => firstNumber * secondNumber,
-  division: (firstNumber: number, secondNumber: number) => firstNumber / secondNumber,
-}

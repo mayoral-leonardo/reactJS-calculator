1188c2c9d2bac6a96b33051ab21534c9ee5b0b94
Leonardo <mayoral.leonardo99@gmail.com>
2023-03-03 09:54:05 -0300

    created Display component

--- src/views/Calculator/Calculator.tsx
+++ src/views/Calculator/Calculator.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Button from "./components/Button/Button";
+import Display from "./components/Display/Display";
 
 export default function Calculator(): React.ReactElement {
   const [result, setResult] = useState<string>('');
@@ -44,28 +45,28 @@ export default function Calculator(): React.ReactElement {
   return (
     <div className="calculator">
       <div className="wrapper">
-        <div className="display">
-          {result !== ''
-            ? <span className="result">{result}</span>
-            : <span className="result">0</span>
-          }
-        </div>
+        <Display result={result}/>
+
         <Button type='special' value='AC' onClick={clear}>AC</Button>
         <Button type='special' value='+/-' onClick={changeSign}>+/-</Button>
         <Button type='special' value='%' onClick={percentage}>%</Button>
         <Button type='operation' value='/' onClick={handleClick}>&divide;</Button>
+
         <Button type='numeric' value='7' onClick={handleClick}>7</Button>
         <Button type='numeric' value='8' onClick={handleClick}>8</Button>
         <Button type='numeric' value='9' onClick={handleClick}>9</Button>
         <Button type='operation' value='*' onClick={handleClick}>&times;</Button>
+
         <Button type='numeric' value='4' onClick={handleClick}>4</Button>
         <Button type='numeric' value='5' onClick={handleClick}>5</Button>
         <Button type='numeric' value='6' onClick={handleClick}>6</Button>
         <Button type='operation' value='-' onClick={handleClick}>&ndash;</Button>
+
         <Button type='numeric' value='1' onClick={handleClick}>1</Button>
         <Button type='numeric' value='2' onClick={handleClick}>2</Button>
         <Button type='numeric' value='3' onClick={handleClick}>3</Button>
         <Button type='operation' value='+' onClick={handleClick}>+</Button>
+        
         <Button type='numeric' value='0' onClick={handleClick}>0</Button>
         <Button type='numeric' value='.' onClick={handleClick}>,</Button>
         <Button type='numeric' value='.' onClick={handleClick} hidden>''</Button>
